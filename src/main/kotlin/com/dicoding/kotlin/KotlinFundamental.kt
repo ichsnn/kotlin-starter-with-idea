7e2d591e1ee69c6a77e59ab8d86e6dfa7e94c457
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2023-02-20 21:13:42 +0700

    Publish

---
+++ src/main/kotlin/com/dicoding/kotlin/KotlinFundamental.kt
@@ -0,0 +1,25 @@
+package com.dicoding.kotlin
+
+// single line comment
+/*
+  multi line comment
+  Hello Kotlin
+ */
+
+fun KotlinFundamental() {
+  val name = "Ichsan"
+
+  print("Hello my name is ")
+  println(name)
+  print(if (true) "Always true" else "Always False")
+  println()
+
+  System.out.print("This is print\n")
+  System.out.println("This is println. You dont need \'n\' to make new line")
+}
+
+/*
+  output:
+    Hello my name is Ichsan
+    Always true
+ */
